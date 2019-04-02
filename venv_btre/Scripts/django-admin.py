1d4d5942c069a858de90364a7357ec13dfef7cc4
Yucehan Kucukmotor <ychnkckmtr@gmail.com>
2019-04-02 13:27:03 -0400

    venv n dir conflict solved

--- venv_btre/Scripts/django-admin.py
+++ venv_btre/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\ychnk\desktop\btre_project\venv_btre\scripts\python.exe
+#!c:\users\yucehan\desktop\btre_project\venv_btre\scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
