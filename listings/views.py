97cc9cbb3b629a7358bf2c01cbeb7b142997f04b
yucehan.kucukmotor <ychnkckmtr@gmail.com>
2019-05-03 18:48:55 -0400

    dynamic featured listings page and pagination added

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
-    """Main index page for all listings"""
-    listings = Listing.objects.all() #Fetch all Listing objects from database
+    """Main index page for all listings that are published.
+       Paginated by 3 listings a page"""
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6) # Show listings per page
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
 
     context = {
-        'listings': listings,
+        'listings': paged_listings
     }
     return render(request, 'listings/listings.html', context)
 
