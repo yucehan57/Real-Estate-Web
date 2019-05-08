eb58368b21c6f0f1c1f17f2338e3077fb2a6bb88
yucehan.kucukmotor <ychnkckmtr@gmail.com>
2019-05-08 11:54:56 -0400

    error message incorporation & extra js for message to fadeout on its own

--- BTRE/settings.py
+++ BTRE/settings.py
@@ -123,4 +123,10 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
 ## pip install psycopg2 and -binary
